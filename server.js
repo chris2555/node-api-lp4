63d35ebba16250194252c11611c45309126abc39
chris2555 <155198892+chris2555@users.noreply.github.com>
2024-07-29 07:12:18 -0600

    port add

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const mysql = require('mysql')
-const myconn = require('express-myconnection')
+const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
 
+const app = express();
 
-const app = express()
-
-app.set('port', process.env.PORT || 3000)
+const port = process.env.PORT || 3000;
 const dbOptions = {
     host: process.env.DB_HOST || "142.44.161.115",
     port: process.env.DB_PORT || "3306",
     user: process.env.DB_USER || "1700Pac2Equ2",
     password: process.env.DB_PASSWORD || "1700Pac2Equ2#65",
     database: process.env.DB_NAME || "1700Pac2Equ2"
-}
+};
 
-app.use(myconn(mysql,dbOptions,'single'))
-app.use(express.json())
+app.use(myconn(mysql, dbOptions, 'single'));
+app.use(express.json());
 
 // Rutas
-app.use(require('./rutas/equipos'))
-app.use(require('./rutas/partidos'))
-app.use(require('./rutas/jugadores'))
-app.use(require('./rutas/estadisticas'))
-app.use(require('./rutas/predicciones'))
-app.use(require('./rutas/estadisticas_equipos'))
+app.use(require('./rutas/equipos'));
+app.use(require('./rutas/partidos'));
+app.use(require('./rutas/jugadores'));
+app.use(require('./rutas/estadisticas'));
+app.use(require('./rutas/predicciones'));
+app.use(require('./rutas/estadisticas_equipos'));
 
+app.listen(port, () => {
+    console.log(`Servidor ejecutándose en el puerto ${port}`);
+});
 
-app.listen(app.get('port'), () =>{
-    console.log('Servidor ejecutándose en el puerto',app.get('port'))
-
-})
